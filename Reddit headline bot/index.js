448c3d27c54ac9e0e95762a2b021fdd7074527b5
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-09-22 23:57:00 +0530

    Updated index.js by removing unnecessary code

--- Reddit headline bot/index.js
+++ Reddit headline bot/index.js
@@ -2,30 +2,7 @@ const rp = require("request-promise");
 const $ = require("cheerio");
 const puppet = require("puppeteer");
 
-//const url = "https://en.wikipedia.org/wiki/Indian_Institutes_of_Information_Technology";
 const url = "https://www.reddit.com/r/dankmemes/";
-
-//let uni_list = [];
-
-// rp(url)
-//   .then(function(html){
-//     //success!
-    
-//     let size = $("td>a", html).length;
-//     for (let i = 0; i<size-1; i++){
-//         if(i%2 === 0){
-//             uni_list.push($("td>a",html)[i].attribs.title);
-//         }
-//     }
-//     console.log("List of Indian institute of information technology");
-//     uni_list.forEach((item)=>{
-//         console.log(item);
-//     })
-//   })
-//   .catch(function(err){
-//     //handle error
-//     console.log(err);
-//   });
 
 let titleList = [];
 
