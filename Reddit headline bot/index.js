de0632a43a01f021a57687883ada1ebac2b27df7
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-09-22 23:55:42 +0530

    Added updated index file

--- Reddit headline bot/index.js
+++ Reddit headline bot/index.js
@@ -1,5 +1,3 @@
-//Work in progress --
-
 const rp = require("request-promise");
 const $ = require("cheerio");
 const puppet = require("puppeteer");
@@ -7,7 +5,7 @@ const puppet = require("puppeteer");
 //const url = "https://en.wikipedia.org/wiki/Indian_Institutes_of_Information_Technology";
 const url = "https://www.reddit.com/r/dankmemes/";
 
-let uni_list = [];
+//let uni_list = [];
 
 // rp(url)
 //   .then(function(html){
@@ -29,20 +27,35 @@ let uni_list = [];
 //     console.log(err);
 //   });
 
+let titleList = [];
+
 puppet
     .launch()
     .then(function(browser){
         return browser.newPage();
     })
     .then(function(page){
-        return page.goto(url)
+        return page.goto(url, { waitUntil: 'networkidle0' })
             .then(function(){
+                page.evaluate(() => {
+                    window.scrollBy(0, document.body.clientHeight);
+                })
                 return page.content();
             })
     })
     .then(function(html){
-        console.log();
+        console.log("Start");
+        $("div[class=_1rZYMD_4xY3gRcSS3p8ODO]","._23h0-EcaBUorIHC-JZyh6J",html).each(function(elem){
+            //console.log($(this).text());
+            titleList.push($(this).text());
+        });
+        titleList.forEach(element => {
+            console.log(element);
+        });
+        //console.log($("div[class=_1rZYMD_4xY3gRcSS3p8ODO]",".rpBJOHq2PR60pnwJlUyP0",html).html());
     })
 
 // upvotes div class = _1rZYMD_4xY3gRcSS3p8ODO
 // arrow and upvotes div id = vote-arrows-t3_itzx8t
+// div class for whole card collection = rpBJOHq2PR60pnwJlUyP0
+// class of h3 for titles = _eYtD2XCVieq6emjKBH3m
